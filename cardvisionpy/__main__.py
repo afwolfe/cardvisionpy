5c90ea80e27fc7baa2be288963e51432a98935af
afwolfe <3718652+afwolfe@users.noreply.github.com>
2022-07-25 19:09:14 -0500

    Add help messages to main

--- cardvisionpy/__main__.py
+++ cardvisionpy/__main__.py
@@ -12,13 +12,12 @@ from cardvisionpy import cardvisionpy
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--input-path", default="images/")
-    argparser.add_argument("--output-file", default="transactions.csv")
-    argparser.add_argument("--verbose", action="store_true")
+    argparser.add_argument("--input-path", default="images/", help="The directory to read image files from. Defaults to ./images/")
+    argparser.add_argument("--output-file", default="transactions.csv", help="The CSV file to write transactions to. Defaults to ./transactions.csv")
 
     args = argparser.parse_args()
     input_path = os.path.abspath(args.input_path)
     output_file = os.path.abspath(args.output_file)
-    
+
     transactions = cardvisionpy.get_processed_transactions(input_path)
     cardvisionpy.write_to_csv(transactions, output_file)
