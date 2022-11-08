b328c23165e0722d6e54c2c0c2548fabca46bb3e
afwolfe <3718652+afwolfe@users.noreply.github.com>
2022-11-08 16:54:03 -0500

    Add CLI to pyproject.toml

--- cardvisionpy/__main__.py
+++ cardvisionpy/__main__.py
@@ -10,7 +10,7 @@ import os
 
 from cardvisionpy import cardvisionpy
 
-if __name__ == "__main__":
+def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--input-path", default="images/", help="The directory to read image files from. Defaults to ./images/")
     argparser.add_argument("--output-file", default="transactions.csv", help="The CSV file to write transactions to. Defaults to ./transactions.csv")
@@ -21,3 +21,6 @@ if __name__ == "__main__":
 
     transactions = cardvisionpy.get_processed_transactions(input_path)
     cardvisionpy.write_to_csv(transactions, output_file)
+    
+if __name__ == "__main__":
+    main()
